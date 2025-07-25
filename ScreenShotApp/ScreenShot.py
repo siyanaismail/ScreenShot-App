3e1e4f94fe67f0f1e720107b83fa4832cc2c2a52
siyanaismail <siyanamismail@gmail.com>
2025-07-25 21:11:51 +0530

    initial commit

--- ScreenShotApp/ScreenShot.py
+++ ScreenShotApp/ScreenShot.py
@@ -3,11 +3,17 @@ import pyautogui
 import tkinter as tk
 
 def screenshot():
+    import os
+    from tkinter import messagebox
+    screenshots_dir = 'C:/Users/HP/Desktop/Udemy Python/ScreenshotApp/screenshots'
+    if not os.path.exists(screenshots_dir):
+        os.makedirs(screenshots_dir)
     name = int(round(time.time() * 1000))
-    name = 'C:/Users/HP/Desktop/Udemy Python/screenshots/{}.png'.format(name)
-    time.sleep(5)
+    name = os.path.join(screenshots_dir, '{}.png'.format(name))
+    time.sleep(1)  # Shorter delay for better UX
     img = pyautogui.screenshot(name)
     img.show()
+    messagebox.showinfo('Screenshot', f'Screenshot saved as:\n{name}')
 
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -19,6 +25,4 @@ close = tk.Button(frame, text='QUIT', command=quit)
 close.pack(side=tk.LEFT)
 
 root.mainloop()
-screenshot()
-
 
